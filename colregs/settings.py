60722a82d390557c88f383eb86030c499dde5509
Dimedresku <zaharov_dima@yahoo.com>
2019-09-02 00:00:13 +0300

    debug false

--- colregs/settings.py
+++ colregs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'hht&mczze*cngn()l&-lggci2f2n5v
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = False
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
