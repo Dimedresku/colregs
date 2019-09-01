5146d5f3c947a7a38cc2fea6dee6c42866c6be8f
Dimedresku <zaharov_dima@yahoo.com>
2019-09-01 22:43:55 +0300

    new settings

--- colregs/settings.py
+++ colregs/settings.py
@@ -127,9 +127,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
