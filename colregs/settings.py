fc1a76b24b22fa38b1063e948e18039d57984a82
Dimedresku <zaharov_dima@yahoo.com>
2019-09-02 11:13:02 +0300

    remove Web rest

--- colregs/settings.py
+++ colregs/settings.py
@@ -134,7 +134,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        #'rest_framework.renderers.BrowsableAPIRenderer',
     ]
 }
 
